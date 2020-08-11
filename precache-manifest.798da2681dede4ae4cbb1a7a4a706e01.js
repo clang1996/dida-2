1ffe2a55095c8e74ac5538eebe677c22cecc6e75
你çclang1996 <1430125208@qq.com>
2020-08-11 18:18:36 +0800

    deploy

---
+++ precache-manifest.798da2681dede4ae4cbb1a7a4a706e01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ef680350a77569df06",
+    "url": "/dida-2-website/css/app.6bb42ef8.css"
+  },
+  {
+    "revision": "7a433d5ceceaa426236b6c94b52938cb",
+    "url": "/dida-2-website/img/logogo22.7a433d5c.png"
+  },
+  {
+    "revision": "23bc76f4724c2a33ae97a474242b8367",
+    "url": "/dida-2-website/index.html"
+  },
+  {
+    "revision": "14ef680350a77569df06",
+    "url": "/dida-2-website/js/app.cdb8aef6.js"
+  },
+  {
+    "revision": "d233ef3c9b3c2c8263f4",
+    "url": "/dida-2-website/js/chunk-vendors.ffb21e19.js"
+  },
+  {
+    "revision": "87e36ed761a5ce601b87d050620f259b",
+    "url": "/dida-2-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dida-2-website/robots.txt"
+  }
+]);
